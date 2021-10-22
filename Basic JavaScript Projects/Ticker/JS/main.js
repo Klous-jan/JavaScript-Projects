059f368a7a564e1b167ab03a8d8d0621149df2eb
Klous-jan <razer.nodder@gmail.com>
2021-10-22 14:33:19 -0700

    Temp files
    Added Calculator project

--- Basic JavaScript Projects/Ticker/JS/main.js
+++ Basic JavaScript Projects/Ticker/JS/main.js
@@ -1,16 +1,33 @@
-var clicks = document.getElementById("steps").innerHTML.value;
+var clicks = document.getElementById("steps").value;
+console.log(clicks)
 
 function onClickplus() {
-  clicks = clicks + 1;
+  clicks = parseInt(clicks) + 1;
   document.getElementById("clicks").innerHTML = clicks;
+  document.getElementById("steps").innerHTML = clicks;
 }
 
 function onClickminus() {
-  clicks = clicks - 1;
+  clicks = parseInt(clicks) - 1;
   document.getElementById("clicks").innerHTML = clicks;
+  document.getElementById("steps").innerHTML = clicks;
+  if(clicks == 0) {
+    document.getElementById('sound').play();
+    alert("You're Done for the day!!");
+  }
 }
 
-function steps()    {
-    var input = document.getElementById("steps").innerHTML.value;
-    document.getElementById("clicks").innerHTML = parseFloat(input);
+function onClickzero() {
+  clicks = parseInt(clicks) + 0;
+  document.getElementById("clicks").innerHTML = clicks;
+  document.getElementById("steps").innerHTML = clicks;
+}
+
+function onInput()  {
+  clicks = document.getElementById("steps").value;
+}
+
+function submit() {
+  document.getElementById("total").innerHTML = clicks;
+  document.getElementById("steps").innerHTML = "";
 }
